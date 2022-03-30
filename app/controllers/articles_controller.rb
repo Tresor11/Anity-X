0edbced47f2f808edb35aa59b4a5cfed3f8937be
Tresor11 <bireketresor@gmail.com>
2022-03-30 09:30:13 +0200

    create the article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,32 @@
 class ArticlesController < ApplicationController
+
+  before_action :require_login , only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
+  def new
+    @article = current_user.articles.build
+  end
+
+  def create
+    @article = current_user.articles.build.build(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 
 end
